8f860652e29b799e94fdf9a9dfb006d7aaa75054
Matthew Wong <90123583+matt-wong-317@users.noreply.github.com>
2023-04-08 14:55:55 -0400

    Editing files

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -12,9 +12,26 @@ function newRoom(){
         .then(response => response.text())
         .then(response => enterRoom(response)); // enter the room with the code
 }
-function enterRoom(code){
 
-    // refresh the list of rooms
+function generatingRandomUpperAlphanumericString(number) {
+    
+}
+
+function enterRoom(){
+
+    //let code = document.getElementById("room-code").value;
+    //console.log(code);
+
+
+    if (document.getElementById("room-code").value == "") {
+
+        // refresh the list of rooms
+        newRoom();
+    }
+    else {
+        let code = document.getElementById("room-code").value;
+        console.log(code);
+    }
 
     // create the web socket
     ws = new WebSocket("ws://localhost:8080/WSChatServer-1.0-SNAPSHOT/ws/"+code);
@@ -25,11 +42,22 @@ function enterRoom(code){
         console.log(event.data);
         // parsing the server's message as json
         let message = JSON.parse(event.data);
-
+        document.getElementById("log").value += "[" + timestamp() + "] " + message.message + "\n";
         // handle message
 
         }
 }
 
+document.getElementById("input").addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        let request = {"type": "chat", "msg": event.target.value};
+        ws.send(JSON.stringify(request));
+        event.target.value = "";
+    }
+});
 
-
+function timestamp() {
+    var d = new Date(), minutes = d.getMinutes();
+    if (minutes < 10) minutes = '0' + minutes;
+    return d.getHours() + ':' + minutes;
+}
