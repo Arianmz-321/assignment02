fcc93eb7a5fb4341ff7db6461deed2af39a6f2bd
Arian M Zerdeh-del <Arian20052001@gmail.com>
2023-04-09 14:20:34 -0400

    Update ChatRoom.java
    Added user class and new methods

--- src/main/java/com/example/webchatserver/ChatRoom.java
+++ src/main/java/com/example/webchatserver/ChatRoom.java
@@ -10,10 +10,49 @@ import java.util.Map;
  * You may add more method or attributes as needed
  * **/
 public class ChatRoom {
-    private String  code;
 
+    public class User {
+        private String id;
+        private String name;
+        private Session session;
+    
+        public User(String id, String name, Session session) {
+            this.id = id;
+            this.name = name;
+            this.session = session;
+        }
+    
+        public String getId() {
+            return id;
+        }
+    
+        public void setId(String id) {
+            this.id = id;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        public Session getSession() {
+            return session;
+        }
+    
+        public void setSession(Session session) {
+            this.session = session;
+        }
+    }
+
+    private String  code;
+    private static List<ChatRoom> rooms = new ArrayList<>();
+   
     //each user has an unique ID associate to their ws session and their username
     private Map<String, String> users = new HashMap<String, String>() ;
+    private List<Session> sessions  = new ArrayList<>() ;
 
     // when created the chat room has at least one user
     public ChatRoom(String code, String user){
@@ -21,6 +60,7 @@ public class ChatRoom {
         // when created the user has not entered their username yet
         this.users.put(user, "");
     }
+
     public void setCode(String code) {
         this.code = code;
     }
@@ -31,6 +71,10 @@ public class ChatRoom {
 
     public Map<String, String> getUsers() {
         return users;
+    }
+
+    public static void add(ChatRoom chatRoom) {
+        rooms.add(chatRoom);    
     }
 
     /**
@@ -49,6 +93,12 @@ public class ChatRoom {
     /**
      * This method will remove a user from this room
      * **/
+
+    public void addUser(String id, String name, Session session) {
+        User newUser = new User(id, name, session);
+        this.sessions.add(session);
+    }
+     
     public void removeUser(String userID){
         if(users.containsKey(userID)){
             users.remove(userID);
