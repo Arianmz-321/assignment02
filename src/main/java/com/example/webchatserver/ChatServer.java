ccb02440d05ca7f86f3d7261b893c7bf2628c406
Matthew Wong <90123583+matt-wong-317@users.noreply.github.com>
2023-04-08 20:18:45 -0400

    Editing files

--- src/main/java/com/example/webchatserver/ChatServer.java
+++ src/main/java/com/example/webchatserver/ChatServer.java
@@ -6,12 +6,14 @@ import jakarta.websocket.server.PathParam;
 import jakarta.websocket.server.ServerEndpoint;
 import org.json.JSONObject;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.example.util.ResourceAPI.loadChatRoomHistory;
-import static com.example.util.ResourceAPI.saveChatRoomHistory;
+//import static com.example.util.ResourceAPI.loadChatRoomHistory;
+//import static com.example.util.ResourceAPI.saveChatRoomHistory;
 
 
 /**
@@ -32,7 +34,8 @@ public class ChatServer {
     @OnOpen
     public void open(@PathParam("roomID") String roomID, Session session) throws IOException, EncodeException {
 
-        session.getBasicRemote().sendText("First sample message to the client");
+        session.getBasicRemote().sendText("Join a room to start playing/chatting.");
+        session.getBasicRemote().sendText("Alphanumeric code:" + roomID);
 //        accessing the roomID parameter
         System.out.println(roomID);
 
@@ -55,8 +58,77 @@ public class ChatServer {
 
     }
 
-    private String loadChatRoomHistory(String roomID) {
-        return roomID;
+    public static void saveChatRoomHistory(String roomID, String log) throws IOException {
+        //String uriAPI = "http://localhost:8080/ChatResourceAPI-1.0-SNAPSHOT/api/history/"+roomID;
+        String uriAPI = "http://localhost:8080/WSChatServer-1.0-SNAPSHOT/api/history/"+roomID;
+        URL url = new URL(uriAPI);
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        // allows us to write content to the outputStream
+        con.setDoOutput(true);
+
+        // sending the data with the POST request
+        String jsonInputString = "{\"room\":\""+roomID+"\",\"log\":\""+log+"\"}";
+        System.out.println(jsonInputString);
+        try(OutputStream os = con.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //reading and printing response
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(con.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    private String loadChatRoomHistory(String roomID) throws IOException {
+        //String uriAPI = "http://localhost:8080/ChatResourceAPI-1.0-SNAPSHOT/api/history/"+roomID;
+        String uriAPI = "http://localhost:8080/WSChatServer-1.0-SNAPSHOT/api/history/"+roomID;
+        URL url = new URL(uriAPI);
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        // we dont need to write content to the outputStream
+        con.setDoOutput(false);
+        // allows us to read inputstream
+        con.setDoInput(true);
+
+        // getting the inputStream
+        InputStream inStream = con.getInputStream();
+        BufferedReader in = new BufferedReader(new InputStreamReader(inStream));
+
+        StringBuffer buffer = new StringBuffer();
+        String line;
+        while ((line = in.readLine()) != null) {
+            buffer.append(line);
+        }
+        String jsonData = buffer.toString();
+
+        System.out.println("load the data");
+        System.out.println(jsonData);
+
+        // transforming the string into objects using org.json library
+        JSONObject data = new JSONObject(jsonData);
+        Map<String, Object> mapData = data.toMap();
+        String content = (String) mapData.get("log");
+
+        return content;
+
     }
 
     @OnClose
