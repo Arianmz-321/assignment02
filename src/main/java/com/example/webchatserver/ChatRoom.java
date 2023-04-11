738edfc0c77ee6bfc3f7574147c463dbe82c0a5f
Matthew Wong <90123583+matt-wong-317@users.noreply.github.com>
2023-04-10 20:32:00 -0400

    Editing files

--- src/main/java/com/example/webchatserver/ChatRoom.java
+++ src/main/java/com/example/webchatserver/ChatRoom.java
@@ -1,66 +1,29 @@
 package com.example.webchatserver;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.websocket.Session;
 
-/**
- * This class represents the data you may need to store about a Chat room
- * You may add more method or attributes as needed
- * **/
+ //This class represents the data you may need to store about a Chat room
+ //You may add more method or attributes as needed
 public class ChatRoom {
 
-    public class User {
-        private String id;
-        private String name;
-        private Session session;
-    
-        public User(String id, String name, Session session) {
-            this.id = id;
-            this.name = name;
-            this.session = session;
-        }
-    
-        public String getId() {
-            return id;
-        }
-    
-        public void setId(String id) {
-            this.id = id;
-        }
-    
-        public String getName() {
-            return name;
-        }
-    
-        public void setName(String name) {
-            this.name = name;
-        }
-    
-        public Session getSession() {
-            return session;
-        }
-    
-        public void setSession(Session session) {
-            this.session = session;
-        }
-    }
+    private String code;
 
-    private String  code;
-    private static List<ChatRoom> rooms = new ArrayList<>();
-   
     //each user has an unique ID associate to their ws session and their username
-    private Map<String, String> users = new HashMap<String, String>() ;
-    private List<Session> sessions  = new ArrayList<>() ;
+    private Map<String, String> users = new HashMap<String, String>();
 
     // when created the chat room has at least one user
-    public ChatRoom(String code, String user){
+    public ChatRoom(String code, String user) {
         this.code = code;
         // when created the user has not entered their username yet
         this.users.put(user, "");
+    }
+
+    public static void add(ChatRoom chatRoom) {
     }
 
     public void setCode(String code) {
@@ -76,40 +39,28 @@ public class ChatRoom {
     }
 
 
-    public static void add(ChatRoom chatRoom) {
-        rooms.add(chatRoom);    
-    }
+    //This method will add the new userID to the room if not exists, or it will add a new userID,name pair
 
-    /**
-     * This method will add the new userID to the room if not exists, or it will add a new userID,name pair
-     * **/
     public void setUserName(String userID, String name) {
         // update the name
-        if(users.containsKey(userID)){
+        if (users.containsKey(userID)) {
             users.remove(userID);
             users.put(userID, name);
-        }else{ // add new user
+        } else { // add new user
             users.put(userID, name);
         }
     }
 
-    /**
-     * This method will remove a user from this room
-     * **/
-
-    public void addUser(String id, String name, Session session) {
-        User newUser = new User(id, name, session);
-        this.sessions.add(session);
-    }
-     
-    public void removeUser(String userID){
-        if(users.containsKey(userID)){
+    //This method will remove a user from this room
+    public void removeUser(String userID) {
+        if (users.containsKey(userID)) {
             users.remove(userID);
         }
 
     }
 
-    public boolean inRoom(String userID){
+    public boolean inRoom(String userID) {
         return users.containsKey(userID);
     }
+
 }
